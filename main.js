5b59a2662a831cb067d754802f7609c2d5b7c52a
Alessandro <marzucoalessandro1@gmail.com>
2021-01-12 12:41:09 +0100

    funzione per ottenere una risposta dalla mia richiesta film

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
 // creiamo istanza di vue
 
 let app = new Vue({
-  
+  el: "#root",
+  // nei nostri dati inseriremo la API key generata
+  // inoltre tramite v-model prenderemo la strnga film per la ricerca
+
+  data: {
+    apiKey: "4f0c8e4dcdaf00015ee0cf645edae2b2",
+    movie: "",
+    myNewFilmList: [],
+    query: "&query=",
+    uri: "https://api.themoviedb.org/3/search/movie?api_key=",
+    newSearch: ""
+  },
+
+  methods: {
+    getList(){
+      // trasformiamo in stringa di ricerca uri + apiKey + movie
+      this.newSearch = this.uri + this.apiKey + this.query + this.movie;
+      axios.get(this.newSearch)
+      .then(item => {
+        this.myNewFilmList = item.data.results
+
+      })
+    }
+  }
+
 });
